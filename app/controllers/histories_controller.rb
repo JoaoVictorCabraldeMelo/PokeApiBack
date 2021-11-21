b6130efde29e11dc0735005414855be17c9cf4b0
JoaoVictorCabraldeMelo <joao.victor.cabralmelo@gmail.com>
2021-11-21 11:34:19 -0300

    🐛 fix: Pokemons repeated in history

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -8,8 +8,8 @@ class HistoriesController < ApplicationController
     def create
         player1 = Player.create!()
         player2 = Player.create!()
-        player1.pokemons << Pokemon.where(id: params[:pokemon_player1_ids])
-        player2.pokemons << Pokemon.where(id: params[:pokemon_player2_ids])
+        player1.pokemons << params[:pokemon_player1_ids].map { |id| Pokemon.find(id) }
+        player2.pokemons << params[:pokemon_player2_ids].map { |id| Pokemon.find(id) }
         history = History.create!(player1_id: player1.id, player2_id: player2.id)    
         render json: history
     end
